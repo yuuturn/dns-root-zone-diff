3c43bbb20cd8c59b9deb2ed11a106aa3ab80eaf7
yuta fujii <mail@yfujii.net>
2026-07-24 04:36:58 +0900

    feat: add config flag parsing and basic main structure

--- cmd/dns-root-diff/main.go
+++ cmd/dns-root-diff/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/yfujii/dns-root-diff/internal/config"
+)
 
 func main() {
-	fmt.Println("dns-root-diff")
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	configPath := flag.String("config", "", "path to config file")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		return fmt.Errorf("load config: %w", err)
+	}
+
+	fmt.Printf("config loaded: zone_url=%s, interval=%v, data_dir=%s\n", cfg.ZoneURL, cfg.FetchInterval, cfg.DataDir)
+	return nil
 }
